30c2a07f31a61fd506155a5b25eb2a60ed4e5917
AMIR <amirmodirizadeh@gmail.com>
2026-06-27 08:02:03 +0330

    Modularize TCP SYN scanner into reusable functions

--- scanner/scans/syn_scan.py
+++ scanner/scans/syn_scan.py
@@ -1,17 +1,28 @@
 from scapy.layers.inet import IP, TCP
 from scapy.sendrecv import sr1, send
 
-ip = "127.0.0.1"
+TARGET = "127.0.0.1"
 
-i = int(input("Enter your port: "))
+def single_scan(port:int):
+    response = sr1(IP(dst=TARGET)/TCP(dport=port, flags="S"), timeout=2, verbose=0)
 
-response = sr1(IP(dst=ip)/TCP(dport=i, flags="S"), timeout=2, verbose=0)
+    if response is None:
+        print("port filtered")
+    elif response.haslayer(TCP):
+        if response[TCP].flags == "SA":
+            print(f"port {port} is open")
+            send(IP(dst=TARGET)/TCP(dport=port, flags="R"), verbose=0)
+        elif response[TCP].flags == "RA" or response[TCP].flags == "R":
+            print(f"port {port} closed")
 
-if response is None:
-    print("port filtered")
-elif response.haslayer(TCP):
-    if response[TCP].flags == "SA":
-        print(f"port {i} is open")
-        send(IP(dst=ip)/TCP(dport=i, flags="R"), verbose=0)
-    elif response[TCP].flags == "RA" or response[TCP].flags == "R":
-        print(f"port {i} closed")
+def range_scan(begin:int, end:int):
+    for port in range(begin, end + 1):
+        response = sr1(IP(dst=TARGET)/TCP(dport=port, flags="S"), timeout=2, verbose=0)
+        if response is None:
+            print("port filtered")
+        elif response.haslayer(TCP):
+            if response[TCP].flags == "SA":
+                print(f"port {port} is open")
+                send(IP(dst=TARGET)/TCP(dport=port, flags="R"), verbose=0)
+            elif response[TCP].flags == "RA" or response[TCP].flags == "R":
+                print(f"port {port} closed")    
